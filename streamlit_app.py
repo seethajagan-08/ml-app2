2f47b6a738e48aa241cff980f0e5cc6a6a95c1ed
Seetha Jagan <regd.asseetha@gmail.com>
2025-12-10 13:26:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def main():
     result=""
     if st.button("Predict"):
         result=predict_authentication(tv, radio, newspaper)
-    st.success('Sales Revenue in {}M$'.format(result))
+    st.success('Sales Revenue in {}M$'.format(result.round(2)))
     if st.button("About Me"):
         st.text("I'm Seetha Jagan")
         st.text("Follow me on 👇")
